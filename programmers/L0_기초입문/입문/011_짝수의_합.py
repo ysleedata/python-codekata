6e3eced9e9e8a4895dca4bae899881b543bba168
ysleedata <yslee041388@gmail.com>
2026-02-23 09:42:57 +0900

    [프로그래머스] L0 - 짝수의 합 (#11)

---
+++ programmers/L0_기초입문/입문/011_짝수의_합.py
@@ -0,0 +1,9 @@
+# 짝수의 합
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120831
+# 알고리즘: 기초
+# 작성자: 이윤서
+# 작성일: 2026. 02. 23. 09:42:56
+
+def solution(n):
+    return sum(range(0, n+1, 2))
